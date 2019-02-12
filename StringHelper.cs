b4fb20ed78b88b4a5664311ce402ff8c13a5c1c0
mxmstfnk <boss.canta@mail.ru>
2019-02-12 17:49:33 +0300

    1

---
+++ StringHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PadawansTask4
+{
+    public class StringHelper
+    {
+        public static int GetVowelCount(string str)
+        {
+            // put your code here
+            int count = 0;
+            for (int i = 0; i < str.Length; ++i)
+            {
+                if (str[i] == 'a' || str[i] == 'e' || str [i] == 'i' || str[i] == 'o' || str[i] == 'u')
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
